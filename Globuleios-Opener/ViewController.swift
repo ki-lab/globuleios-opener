d19dc8c50d7bdd3a7ee35595b6afe970342a25ae
Antonin Molieres <molieres.antonin@gmail.com>
2019-06-19 16:07:16 +0200

    Minor improvements
    - naming rectifications
    - exclude simulator use

--- Globuleios-Opener/ViewController.swift
+++ Globuleios-Opener/ViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let globuleSheme = "globule://"
+    private let globuleScheme = "globule://"
     private let globuleItunesUrl = "https://apps.apple.com/fr/app/globule/id432735249"
     
     @IBAction func openGlobule(_ sender: Any) {
         
-        if let globuleUrl = URL(string: globuleSheme), UIApplication.shared.canOpenURL(globuleUrl) {
+        var alert: UIAlertController?
+        
+        #if targetEnvironment(simulator)
+    
+        alert = UIAlertController(title: "Erreur", message: "Cette application doit être lancé depuis un veritable appareil", preferredStyle: .alert)
+        alert?.addAction(UIAlertAction(title: "Ok", style: .default))
+        
+        #else
+        
+        if let globuleUrl = URL(string: globuleScheme), UIApplication.shared.canOpenURL(globuleUrl) {
             // We can open Globule scheme
             UIApplication.shared.open(globuleUrl, options: [:])
             
@@ -23,22 +32,24 @@ class ViewController: UIViewController {
         } else if let globuleAppStoreUrl = URL(string: globuleItunesUrl), UIApplication.shared.canOpenURL(globuleAppStoreUrl) {
             
             // We can't open Globule scheme -> Redirect user to AppStore
-            let alert = UIAlertController(title: "Erreur", message: "Globule n'est pas installé", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Annuler", style: .cancel))
-            alert.addAction(UIAlertAction(title: "AppStore", style: .default, handler: { _ in
+            alert = UIAlertController(title: "Erreur", message: "Globule n'est pas installé", preferredStyle: .alert)
+            alert?.addAction(UIAlertAction(title: "Annuler", style: .cancel))
+            alert?.addAction(UIAlertAction(title: "AppStore", style: .default, handler: { _ in
                 UIApplication.shared.open(globuleAppStoreUrl, options: [:], completionHandler: nil)
             }))
-            self.present(alert, animated: true, completion: nil)
             
             
         } else {
             // AppStore link missing, show an error
-            let alert = UIAlertController(title: "Erreur", message: "Impossible d'ouvrir Globule", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .default))
-            self.present(alert, animated: true, completion: nil)
+            alert = UIAlertController(title: "Erreur", message: "Impossible d'ouvrir Globule", preferredStyle: .alert)
+            alert?.addAction(UIAlertAction(title: "Ok", style: .default))
             
         }
-
+        #endif
+        
+        if let alert = alert {
+            self.present(alert, animated: true, completion: nil)
+        }
     }
     
 }
